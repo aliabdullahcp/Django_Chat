34ef6ad4a152f3cd3fbf43593101b085b8489414
Ali Abdullah <aliabdullahcp@hotmail.com>
2021-05-20 12:35:33 -0700

    Adding disconnect fn

--- Chat/chat/consumers.py
+++ Chat/chat/consumers.py
@@ -12,4 +12,9 @@ class ChatRoomConsumer(AsyncWebsocketConsumer):
 
         )
 
+    async def disconnect(self, close_code):
+        await self.channel_layer.group_discard(
+            self.room_group_name,
+            self.channel_name
+        )
     pass
